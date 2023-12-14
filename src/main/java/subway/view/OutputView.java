9c2a299819e8ba4bba5a54f6f1107b5a2ac02c27
Gyeongho Yang <chchch1213@naver.com>
2023-12-14 17:02:28 +0900

    feat(view): 조회 결과 출력 기능 구현

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -2,9 +2,10 @@ package subway.view;
 
 import subway.constant.GeneralMessage;
 
+import java.util.List;
+
 import static subway.constant.ErrorMessage.EXCEPTION_PREFIX;
-import static subway.constant.GeneralMessage.NOTIFY_MAIN;
-import static subway.constant.GeneralMessage.NOTIFY_WEIGHT_CONDITION;
+import static subway.constant.GeneralMessage.*;
 
 public class OutputView {
 
@@ -23,6 +24,18 @@ public class OutputView {
 
     public static void printWeightCondition() {
         printGeneralMessage(NOTIFY_WEIGHT_CONDITION);
+        System.out.println();
+    }
+
+    public static void printResult(int distance, int time, List<String> stations) {
+        printGeneralMessage(NOTIFY_RESULT);
+        printGeneralMessage(NOTIFY_INFO, NOTIFY_LINE_SEPARATOR.toString());
+        printGeneralMessage(NOTIFY_INFO, String.format(NOTIFY_TOTAL_DISTANCE.toString(), distance));
+        printGeneralMessage(NOTIFY_INFO, String.format(NOTIFY_TOTAL_TIME.toString(), time));
+        printGeneralMessage(NOTIFY_INFO, NOTIFY_LINE_SEPARATOR.toString());
+        stations.forEach(station -> {
+            printGeneralMessage(NOTIFY_INFO, station);
+        });
         System.out.println();
     }
 }
