d2ed5242775fcebd0bd6ba5392001b9e8e6cb572
Gyeongho Yang <chchch1213@naver.com>
2023-12-14 16:43:17 +0900

    feat(const): 역 입력시의 예외 상황 메세지 추가

--- src/main/java/subway/constant/ErrorMessage.java
+++ src/main/java/subway/constant/ErrorMessage.java
@@ -2,7 +2,10 @@ package subway.constant;
 
 public enum ErrorMessage {
     EXCEPTION_PREFIX("[ERROR] "),
-    INVALID_FUNCTION("해당 기능은 존재하지 않습니다. 다시 입력해 주세요.");
+    INVALID_FUNCTION("해당 기능은 존재하지 않습니다. 다시 입력해 주세요."),
+    INVALID_STATION("해당 역은 존재하지 않습니다. 다시 입력해 주세요."),
+    SAME_STATION_DEPART_AND_DEST("출발역과 도착역이 동일합니다. 다시 입력해 주세요."),
+    NO_LINE_FOUND("출발역과 도착역이 동일합니다. 다시 입력해 주세요.");
 
     private final String message;
 
