b8876c7e25b4eeaed04cbe54191bf09bb27d55c4
Gyeongho Yang <chchch1213@naver.com>
2023-12-14 17:01:46 +0900

    feat(const): 조회 결과 출력을 위한 메세지 추가

--- src/main/java/subway/constant/GeneralMessage.java
+++ src/main/java/subway/constant/GeneralMessage.java
@@ -3,8 +3,15 @@ package subway.constant;
 public enum GeneralMessage {
     NOTIFY_MAIN("## 메인 화면\n1. 경로 조회\nQ. 종료"),
     NOTIFY_WEIGHT_CONDITION("## 경로 기준\n1. 최단 거리\n2. 최소 시간\nB. 돌아가기"),
+    NOTIFY_RESULT("## 조회 결과"),
+    NOTIFY_INFO("[INFO] %s"),
+    NOTIFY_LINE_SEPARATOR("---"),
+    NOTIFY_TOTAL_DISTANCE("총 거리: %dkm"),
+    NOTIFY_TOTAL_TIME("총 소요 시간: %d분"),
 
-    REQUEST_FUNCTION("## 원하는 기능을 선택하세요.");
+    REQUEST_FUNCTION("## 원하는 기능을 선택하세요."),
+    REQUEST_DEPARTURE("## 출발역을 입력하세요."),
+    REQUEST_DESTINATION("## 도착역을 입력하세요.");
 
     private final String message;
 
